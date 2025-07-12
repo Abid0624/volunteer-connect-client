2b520615038fd0ec175a8db879faf8646bcd6e57
Abid064 <abidkhan09989@gmail.com>
2025-07-12 20:30:17 +0600

    sorted functionality complete

--- src/Components/HotJobs.jsx
+++ src/Components/HotJobs.jsx
@@ -1,8 +1,34 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import PostCard from "./PostCard";
+import { Link } from "react-router-dom";
 
 const HotJobs = () => {
-  const [jobs, setJobs] = useState([]);
-  return <div>HotJobs</div>;
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchAllPost = async () => {
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_API_URL}/asc-jobs`
+      );
+      setPosts(data);
+    };
+    fetchAllPost();
+  }, []);
+  return (
+    <div className="container px-6 py-10 mx-auto">
+      <h1 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl ">
+        Volunteer Needs Now!
+      </h1>
+      <div className="grid mb-2 grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {posts.map((post) => (
+          <PostCard key={post._id} post={post}></PostCard>
+        ))}
+      </div>
+      <Link to="/all-volunteer">
+        <button className="btn mt-2 btn-secondary">See All</button>
+      </Link>
+    </div>
+  );
 };
 
 export default HotJobs;
