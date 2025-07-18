01e36338fc1161043d231f1c733d9de4f7e14621
Abid064 <abidkhan09989@gmail.com>
2025-07-19 03:47:07 +0600

    axios interceptors implemented

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import useAuth from "./useAuth";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const axiosSecure = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
+
+const useAxiosSecure = () => {
+  const { logOut } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axiosSecure.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      async (error) => {
+        console.log(
+          "error caught from our very own axios interceptors",
+          error.response
+        );
+        if (error.response.status === 401 || error.response.status === 403) {
+          // logout
+          logOut();
+          // navigate to login
+          navigate("/login");
+        }
+      }
+    );
+  }, [logOut, navigate]);
+  return axiosSecure;
+};
+
+export default useAxiosSecure;
